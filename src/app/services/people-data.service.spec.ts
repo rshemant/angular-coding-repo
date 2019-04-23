4e06d350eb99036fc20d753db941481335c0e67b
Hemant Soni <hemantsoni@Hemants-MacBook-Pro.local>
2019-04-24 00:15:50 +0530

    Complete code for people data service with all possible test cases

--- src/app/services/people-data.service.spec.ts
+++ src/app/services/people-data.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { PeopleDataService } from './people-data.service';
 import { Owner } from '../models/owner/owner.model';
+import { Pet } from '../models/pet/pet.model';
 
 import { environment } from '../../environments/environment';
 
@@ -57,4 +58,186 @@ describe('PeopleDataService', () => {
     mockRequest.flush(mockOwners);
   });
 
+  it('Should return owners filtered by gender', async () => {
+    const mockResponse = [
+      {
+        name: "Steve",
+        gender: "Male",
+        age: 45,
+        pets: null
+      },
+      {
+        name: "Jennifer",
+        gender: "Female",
+        age: 18,
+        pets: [
+          {
+            name: "Garfield",
+            type: "Cat"
+          }
+        ]
+      },
+      {
+        name: "Fred",
+        gender: "Male",
+        age: 40,
+        pets: [
+          {
+            name: "Tom",
+            type: "Cat"
+          },
+          {
+            name: "Max",
+            type: "Cat"
+          },
+          {
+            name: "Sam",
+            type: "Dog"
+          },
+          {
+            name: "Jim",
+            type: "Cat"
+          }
+        ]
+      }
+    ];
+
+    service
+      .getPeople()
+      .subscribe((owners: Owner[]) => {
+        // Only Owners That Are Male
+        const onlyMaleOwners = service.getOwnerFilterByGender(owners, 'Male');
+        expect(onlyMaleOwners.every((owner: Owner) => owner.gender === 'Male')).toBeTruthy();
+        expect(onlyMaleOwners.every((owner: Owner) => owner.gender === 'Female')).toBeFalsy();
+
+        // Only Owners That Are Female
+        const onlyFemaleOwners = service.getOwnerFilterByGender(owners, 'Female');
+        
+        expect(onlyFemaleOwners.every((owner: Owner) => owner.gender === 'Female')).toBeTruthy();
+        expect(onlyFemaleOwners.every((owner: Owner) => owner.gender === 'Male')).toBeFalsy();
+
+      });
+
+    const mockReq = httpMock.expectOne(service.RESOURCE_URL);
+    console.log(mockReq);
+    mockReq.flush(mockResponse);
+  });
+
+
+  it('Should combine pets of owners', async () => {
+    const mockResponse = [
+      {
+        name: "Steve",
+        gender: "Male",
+        age: 45,
+        pets: null
+      },
+      {
+        name: "Jennifer",
+        gender: "Female",
+        age: 18,
+        pets: [
+          {
+            name: "Garfield",
+            type: "Cat"
+          }
+        ]
+      },
+      {
+        name: "Fred",
+        gender: "Male",
+        age: 40,
+        pets: [
+          {
+            name: "Tom",
+            type: "Cat"
+          },
+          {
+            name: "Max",
+            type: "Cat"
+          },
+          {
+            name: "Sam",
+            type: "Dog"
+          },
+          {
+            name: "Jim",
+            type: "Cat"
+          }
+        ]
+      }
+    ];
+
+    const expectedPetList : Pet[] = [
+      { name: "Garfield", type: "Cat" },
+      { name: "Tom", type: "Cat" },
+      { name: "Max", type: "Cat" },
+      { name: "Sam", type: "Dog" },
+      { name: "Jim", type: "Cat" }
+    ];
+
+    service
+      .getPeople()
+      .subscribe((owners: Owner[]) => {
+        // Combined list of Pets
+        const AllPets = service.combinePetsOfOwner(owners);
+        expect(AllPets).toEqual(expectedPetList);
+      });
+
+    const mockReq = httpMock.expectOne(service.RESOURCE_URL);
+    console.log(mockReq);
+    mockReq.flush(mockResponse);
+  });
+
+  it('Should sort resulting pet list alphabetically', () => {
+    const mockOwners: Owner[] = [
+      {
+        name: 'Bob',
+        gender: 'Male',
+        age: 23,
+        pets: [
+          { name: 'Zaggy', type: 'Dog' },
+          { name: 'Albert', type: 'Dog' },
+        ]
+      },
+      {
+        name: 'Jennifer',
+        gender: 'Female',
+        age: 32,
+        pets: [
+          { name: 'Mittens', type: 'Cat' },
+          { name: 'Candy', type: 'Cat' },
+        ]
+      },
+    ];
+
+    const expectedMaleOwnedDogNames: string[] = [
+      'Albert',
+      'Zaggy',
+    ];
+
+    const expectedFemaleOwnedCatNames: string[] = [
+      'Candy',
+      'Mittens',
+    ];
+
+    service
+      .getPeople()
+      .subscribe((owners: Owner[]) => {
+        // Pets of Male Owners Are Sorted Alphabetically
+        const onlyMaleOwnedDogs = service
+          .sortPetsByOwnerGenderAndType(owners, 'Male', 'Dog')
+          .map((pet: Pet) => pet.name);
+        expect(onlyMaleOwnedDogs).toEqual(expectedMaleOwnedDogNames);
+
+        // Pets of Female Owners Are Sorted Alphabetically
+        const onlyFemaleOwnedCats = service
+          .sortPetsByOwnerGenderAndType(owners, 'Female', 'Cat')
+          .map((pet: Pet) => pet.name);
+        expect(onlyFemaleOwnedCats).toEqual(expectedFemaleOwnedCatNames);
+      });
+
+    const mockReq = httpMock.expectOne(service.RESOURCE_URL);
+    mockReq.flush(mockOwners);
+  });
 });
--- src/app/services/people-data.service.ts
+++ src/app/services/people-data.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Owner } from 'src/app/models/owner/owner.model';
+import { Owner, Gender } from 'src/app/models/owner/owner.model';
+import { Pet, PetType } from 'src/app/models/pet/pet.model';
 
 import { environment } from '../../environments/environment';
 
@@ -11,12 +12,39 @@ import { environment } from '../../environments/environment';
 })
 export class PeopleDataService {
 
-  RESOURCE_URL = environment.apiUrl;
+    RESOURCE_URL = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  public getPeople(): Observable<Owner[]> {
-    return this.http.get(this.RESOURCE_URL) as Observable<Owner[]>;
-  }
+    public getPeople(): Observable<Owner[]> {
+        return this.http.get(this.RESOURCE_URL) as Observable<Owner[]>;
+    }
+
+    public getOwnerFilterByGender(ownerList: Owner[], gender: Gender): Owner[] {
+        return ownerList.filter((owner: Owner) => owner.gender === gender);
+    }
+
+    public getPetFilterByType(petList: Pet[], petType: PetType): Pet[] {
+        return petList.filter((pet: Pet) => pet.type === petType)
+    }
+
+    public combinePetsOfOwner(ownerList: Owner[]): Pet[] {
+        return ownerList.reduce((petList: Pet[], cur: Owner) => {
+            petList.push(...cur.pets);
+            return petList;
+        }, []);
+    }
+
+    public sortPetsByOwnerGenderAndType(ownerList: Owner[], gender: Gender, petType: PetType): Pet[] {
+
+        let localOwnerList = this.getOwnerFilterByGender(ownerList, gender);
+
+        let localPetList: Pet[];
+
+        localPetList = this.combinePetsOfOwner(localOwnerList);
+        localPetList = this.getPetFilterByType(localPetList, petType);
+
+        return localPetList.sort((petA: Pet, petB: Pet) => petA.name.localeCompare(petB.name));
+    }
 
 }
