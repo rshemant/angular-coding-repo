ead5282da9b9624b56ac9fe54a750cdb1a1014f5
Hemant Soni <hemantsoni@Hemants-MacBook-Pro.local>
2019-04-24 00:54:19 +0530

    app.module.ts updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetListComponent } from './views/pet-list/pet-list.component';
+import { PetInfoComponent } from './views/pet-info/pet-info.component';
+import { InfoPageComponent } from './views/info-page/info-page.component';
+import { PetPageComponent } from './views/pet-page/pet-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetListComponent,
+    PetInfoComponent,
+    InfoPageComponent,
+    PetPageComponent
   ],
   imports: [
     BrowserModule,
