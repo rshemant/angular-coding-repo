6bd68d57b31a9f4b09304d08aabef36679c22591
Hemant Soni <hemantsoni@Hemants-MacBook-Pro.local>
2019-04-24 00:52:48 +0530

    Adding boilerplate code for components

---
+++ src/app/views/pet-info/pet-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-info',
+  templateUrl: './pet-info.component.html',
+  styleUrls: ['./pet-info.component.sass']
+})
+export class PetInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/views/pet-page/pet-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-page',
+  templateUrl: './pet-page.component.html',
+  styleUrls: ['./pet-page.component.sass']
+})
+export class PetPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
